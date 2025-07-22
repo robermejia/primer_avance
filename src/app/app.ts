a7a4fa435e35672ee4a1a4e4c71157d059de3179
Roberto Agustín Mejía Collazos <robertomejiacollazos@gmail.com>
2025-07-22 07:39:11 -0500

    feat: toolbar & navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Toolbar } from './toolbar/toolbar';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Toolbar, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
