af3398cde3060437bb79a6ddb07af872675e63ce
Roberto Agustín Mejía Collazos <robertomejiacollazos@gmail.com>
2025-07-22 09:39:08 -0500

    feat: request mock students by get req

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { Toolbar } from './toolbar/toolbar';
 import { Navbar } from './navbar/navbar';
+import { Student } from '../shared/entities';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Toolbar, Navbar],
+  imports: [Toolbar, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  // Title of the application
   protected readonly title = signal('primer_avance');
+
+  
 }
