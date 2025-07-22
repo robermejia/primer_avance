31067b07be16544a6a12cdaba31c1b5de958a5e1
Roberto Agustín Mejía Collazos <robertomejiacollazos@gmail.com>
2025-07-22 01:12:45 -0500

    feat: provideHttpClient(withFetch())

--- src/app/students-table/students-table.ts
+++ src/app/students-table/students-table.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Student } from '../../shared/entities';
 
 import {MatTableModule} from '@angular/material/table';
+import { FullnamePipe } from '../../shared/pipes/fullname-pipe';
 
 
 @Component({
   selector: 'app-students-table',
-  imports: [MatTableModule],
+  imports: [MatTableModule, FullnamePipe],
   templateUrl: './students-table.html',
   styleUrl: './students-table.css'
 })
